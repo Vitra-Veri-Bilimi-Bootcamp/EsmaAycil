fb70a974cd06e23e512cfa266d089ad4c10ea858
Esma Aycil <43879854+esmaycil@users.noreply.github.com>
2021-07-13 13:55:03 +0300

    Create Odev2.py

---
+++ Patika/Hafta2/Odev2.py
@@ -0,0 +1,10 @@
+#Verilen listenin elemanlarını tersine döndüren fonksiyon
+def ters(liste):
+    liste.reverse()
+    for i in liste:
+        if type(i) is list:
+            ters(i)
+    return liste
+
+myList=[1,2,3,["car1","car2","car3",[8,9,10]],150,152,153]
+print(ters(myList))
